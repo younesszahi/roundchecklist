75b7752795a3008bf7fc395f7bfd325b1e5d9e11
younes zahi <155114289+youneszahi2002@users.noreply.github.com>
2024-07-17 10:23:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
 
-    if (username === 'cdg' && password === 'cdg') {
-        window.location.href = 'form.html'; // Redirect to the form page
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "cdg" && password === "cdg") {
+        window.location.href = "form.html"; // Redirect to the form page
     } else {
-        alert('Incorrect username or password. Please try again.');
+        alert("Invalid username or password. Please try again.");
     }
 });
